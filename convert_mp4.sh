2d17c8e780bf489ea65eb617104142fc899a0139
Kevin Roark <ker2143@columbia.edu>
2015-03-30 20:31:05 -0400

    add mp4 and webm video files

---
+++ convert_mp4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+~/code/art/shane/mistershane/webm.sh video/black_holes.mp4 video/black_holes.webm
+~/code/art/shane/mistershane/webm.sh video/ear_pov.mp4 video/ear_pov.webm
+~/code/art/shane/mistershane/webm.sh video/ehookah.mp4 video/ehookah.webm
+~/code/art/shane/mistershane/webm.sh video/hummus.mp4 video/hummus.webm
+~/code/art/shane/mistershane/webm.sh video/iphone.mp4 video/iphone.webm
+~/code/art/shane/mistershane/webm.sh video/mac.mp4 video/mac.webm
+~/code/art/shane/mistershane/webm.sh video/mcdonalds.mp4 video/mcdonalds.webm
+~/code/art/shane/mistershane/webm.sh video/morning_routine.mp4 video/morning_routine.webm
+~/code/art/shane/mistershane/webm.sh video/papa_johns.mp4 video/papa_johns.webm
+~/code/art/shane/mistershane/webm.sh video/pasta.mp4 video/pasta.webm
+~/code/art/shane/mistershane/webm.sh video/pickles.mp4 video/pickles.webm
+~/code/art/shane/mistershane/webm.sh video/real_hookah.mp4 video/real_hookah.webm
